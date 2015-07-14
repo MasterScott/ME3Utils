9dd02301aa3e11406da223dbcb4f106889b2606d
Heff <heff@heff.se>
2015-07-14 09:54:23 +0200

    Changed to inline ASM version of the printf, now prints a 3-evel stack trace for easier reversing.

--- ME3Log/DllMain.cpp
+++ ME3Log/DllMain.cpp
@@ -31,6 +31,52 @@ void __cdecl LogPrintf(ErrorClass* error, wchar_t* pFormat, ...)
 	fflush(Log);
 }
 
+int return_addr_one = 0;
+int return_addr_two = 0;
+int return_addr_three = 0;
+
+// inline asm version, needed to get stack frame:
+void __declspec(naked) LogPrintf_ASM(void)
+{
+	__asm
+	{
+		push ebp
+		mov ebp, esp			// fetch stack frame
+		push eax				// backup eax
+
+		mov eax, [ebp + 0x4]		// fetch old stackframe's return pointers for easy printing below
+		mov return_addr_one, eax
+		mov eax, [ebp]
+		mov eax, [eax + 0x4]
+		mov return_addr_two, eax
+		mov eax, [ebp]
+		mov eax, [eax]
+		mov eax, [eax + 0x4]
+		mov return_addr_three, eax
+
+		lea eax, [ebp + 0x10]	// forward the va_list
+		push eax
+		mov eax, [ebp + 0xC]	// forward the format  string
+		push eax
+		mov eax, Log			// set print target to our log
+		push eax
+
+		call vfwprintf
+		add esp, 0xC			// clean up stack
+
+	}
+
+	fprintf(Log, " | Stacktrace: ME3Logger <- [%#x] <- [%#x] <- [%#x] \n", 
+		return_addr_one, return_addr_two, return_addr_three);			// print and flush
+	fflush(Log);
+
+	_asm {
+		pop eax					// restore
+		pop ebp
+		retn
+	}
+}
+
 void DetourPrintFunction()
 {
 	BYTE *BioDiagnosticPrintf = (BYTE *)0x00467920;
@@ -39,7 +85,7 @@ void DetourPrintFunction()
 	VirtualProtect(BioDiagnosticPrintf, 0x5, PAGE_READWRITE, &OriginalProtection);
 
 	// calculate relative jump offset
-	DWORD JmpOffset = (DWORD)((DWORD)LogPrintf - (DWORD)BioDiagnosticPrintf) - 5;
+	DWORD JmpOffset = (DWORD)((DWORD)LogPrintf_ASM - (DWORD)BioDiagnosticPrintf) - 5;
 
 	*BioDiagnosticPrintf = 0xE9;					// write the long relative jmp
 	memcpy(BioDiagnosticPrintf + 1, &JmpOffset, 4); // copy the destination adress
